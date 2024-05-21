4b88ebd4784499527826e01cc35f977b122c6128
quang.tranthanh <quang.tranthanh@vnpt.vn>
2024-05-21 15:15:42 +0700

    update axios

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import axios from 'axios';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
 import router from './router'
 const pinia = createPinia()
